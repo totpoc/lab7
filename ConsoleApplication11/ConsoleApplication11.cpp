352d1e1ac592f4fd115b8b3226bd51f59004d560
spais <maksim.ryabcew@gmail.com>
2020-12-14 23:44:57 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int NOD(int a, int b)
+{
+	if (b == 0)
+		return a;
+	else
+		return NOD(b, a % b);
+}
+
+int main()
+{
+
+	int firstNumber, seconNumber;
+	
+	do {
+		cout << "Введите первое число: ";
+		cin >> firstNumber;
+	} while (firstNumber <= 0);
+	
+	do {
+		cout << "Введите второе число: ";
+		cin >> seconNumber;
+	} while (seconNumber <= 0);
+
+	if (seconNumber == 0)
+		cout << firstNumber << endl;
+	else
+		cout << "NOD = " << NOD(firstNumber, seconNumber) << endl;
+
+	
+	return 0;
+}
